029c6ddfb1e9c6314cb672336058c42d39bfdb78
Cameron Wong <cam@lancer>
2015-10-29 11:50:08 -0400

    initial commit stuff

---
+++ edu/andover/cwong/gscq/GSCQRunner.java
@@ -0,0 +1,17 @@
+package edu.andover.cwong.gscq;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GSCQRunner extends Application {
+
+    @Override
+    public void start(Stage s) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    public static void main(String... args) {
+        launch(args);
+    }
+}
