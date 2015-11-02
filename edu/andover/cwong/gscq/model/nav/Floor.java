dc28b144b34b5a0d7dec90bad74337d6a77d1da1
Cameron Wong <cam@lancer>
2015-11-02 11:56:43 -0500

    formatting stuff

--- edu/andover/cwong/gscq/model/nav/Floor.java
+++ edu/andover/cwong/gscq/model/nav/Floor.java
@@ -1,8 +1,13 @@
 package edu.andover.cwong.gscq.model.nav;
+
+import edu.andover.cwong.gscq.model.unit.GameEntity;
+
 public class Floor {
 	private final Tile[][] floorTiles;
+	private final GameEntity[][] units;
 	public Floor(int x, int y) {
 		floorTiles = new Tile[x][y];
+		units = new GameEntity[x][y];
 	}
 	public Tile getTile(int x,int y) {
 		return floorTiles[x][y];
