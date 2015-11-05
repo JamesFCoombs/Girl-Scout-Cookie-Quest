9b12ad7f872930c9a38dcff5859e3ee74310ede1
Cameron Wong <cam@lancer>
2015-11-04 23:09:35 -0500

    runner actually loads something now

--- edu/andover/cwong/gscq/GSCQRunner.java
+++ edu/andover/cwong/gscq/GSCQRunner.java
@@ -3,16 +3,36 @@ package edu.andover.cwong.gscq;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import java.io.IOException;
+
+import edu.andover.cwong.gscq.control.GameController;
 
 public class GSCQRunner extends Application {
     private Stage primaryStage;
     private BorderPane layoutRoot;
     @Override
     public void start(Stage s) throws Exception {
-        
+        this.primaryStage = s;
+        this.primaryStage.setTitle("Girl Scout Cookie Quest!");
+        initRoot();
+        initContainer();
+    }
+    
+    public void initRoot() {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    GSCQRunner.class.getResource("view/RootFrame.fxml"));
+            layoutRoot = loader.load();
+            this.primaryStage.setScene(new Scene(layoutRoot));
+            this.primaryStage.setResizable(false);
+            this.primaryStage.sizeToScene();
+        } catch (IOException e) {
+            System.err.println("Unable to load root layout. Aborting");
+            System.exit(-1);
+        }
     }
     
     public void initContainer() {
@@ -20,8 +40,12 @@ public class GSCQRunner extends Application {
             FXMLLoader loader = new FXMLLoader(
                     GSCQRunner.class.getResource("view/GameContainer.fxml"));
             AnchorPane gameContainer = loader.load();
+            GameController ctrlr = loader.getController();
+            ctrlr.setOwner(this);
+            layoutRoot.setCenter(gameContainer);
+            this.primaryStage.show();
         } catch (IOException e) {
-            System.err.println("Unable to load root layout. Aborting");
+            System.err.println("Unable to load game layout. Aborting");
             System.exit(-1);
         }
     }
--- edu/andover/cwong/gscq/control/GameController.java
+++ edu/andover/cwong/gscq/control/GameController.java
@@ -3,10 +3,17 @@ package edu.andover.cwong.gscq.control;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
+import edu.andover.cwong.gscq.GSCQRunner;
+
 public class GameController {
-    @FXML
-    AnchorPane gamePanel;
+    private GSCQRunner owner;
     
     @FXML
+    AnchorPane gamePanel;
+    @FXML
     AnchorPane hudPanel;
+    
+    public void setOwner(GSCQRunner g) {
+        this.owner = g;
+    }
 }
