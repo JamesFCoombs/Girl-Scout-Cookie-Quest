9ffd74c8be88376095d978062fe40d7d4909db1f
Cameron Wong <cam@lancer>
2015-11-09 22:45:42 -0500

    merge 2

--- src/edu/andover/cwong/gscq/model/unit/Player.java
+++ src/edu/andover/cwong/gscq/model/unit/Player.java
@@ -3,10 +3,6 @@ package edu.andover.cwong.gscq.model.unit;
 import java.util.ArrayList;
 
 import edu.andover.cwong.gscq.model.items.CookieRecipe;
-<<<<<<< HEAD
-=======
-//import edu.andover.cwong.gscq.model.items.Item;
->>>>>>> 52b15ed4214c529ac0e9cf2834245c31c0af279f
 import edu.andover.cwong.gscq.model.items.PlainCookie;
 
 public class Player extends LivingGameEntity {
