b8d0693536e43c74280a43e43db0874061f0b12a
Diana <fding@andover.edu>
2015-11-17 18:54:45 -0500

    made some tests

---
+++ src/edu/andover/cwong/gscq/tests/GSCQTest.java
@@ -0,0 +1,68 @@
+package edu.andover.cwong.gscq.tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class GSCQTest {
+	@Test 
+	public void everyTileConnectsToAtLeastOneOtherTile() {
+		//To-do: I don't know how floor generation works
+	}
+	
+	@Test
+	public void playerDiesWhenHealthIsZero() {
+		//To-do
+	}
+	
+	@Test
+	public void playerCannotGoThroughWall() {
+		//To-do
+	}
+	
+	@Test
+	public void playerCannotBeOnTileImmediatelyBeyondTheWall() {
+		//To-do
+	}
+	
+	@Test
+	public void enemyCannotBeOnTileImmediatelyBeyondWall() {
+		//To-do
+	}
+	
+	@Test
+	public void itemCannotBeOnTileImmediatelyBeyondWall() {
+		//To-do
+	}
+	
+	@Test
+	public void playerAttackCannotBeNegative () {
+		//To-do
+	}
+	
+	@Test
+	public void playerDefenseCannotBeNegative () {
+		//To-do
+	}
+	
+	@Test
+	public void playerHealthCannotBeNegative () {
+		//To-do
+	}
+	
+	@Test
+	public void playerGainsCookieIfMovementReverts () {
+		//To-do
+	}
+	
+	@Test
+	public void playerHealthCannotBeEleven() {
+		//To-do
+	}
+	
+	@Test
+	public void everyLevelHasAnExit() {
+		//To-do
+	}
+}
+
+
