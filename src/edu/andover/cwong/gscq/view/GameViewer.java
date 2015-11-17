4a75a9eb5c1a5ca7a426e7c112cce9c8d9a89719
Cameron Wong <cam@lancer>
2015-11-17 17:02:17 -0500

    can we merge PLEASE

--- src/edu/andover/cwong/gscq/view/GameViewer.java
+++ src/edu/andover/cwong/gscq/view/GameViewer.java
@@ -1,5 +1,8 @@
 package edu.andover.cwong.gscq.view;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
@@ -9,11 +12,10 @@ import javafx.geometry.Rectangle2D;
 
 import edu.andover.cwong.gscq.GSCQRunner;
 import edu.andover.cwong.gscq.view.FloorViewBuilder;
+import edu.andover.cwong.gscq.view.Sprite;
 import edu.andover.cwong.gscq.model.Game;
 import edu.andover.cwong.gscq.model.items.Item;
-import edu.andover.cwong.gscq.model.unit.Enemy;
 import edu.andover.cwong.gscq.model.unit.GameEntity;
-import edu.andover.cwong.gscq.model.unit.Player;
 
 // What JFX calls a "controller" for the game panel. Handles refreshing of
 // view elements (etc)
@@ -21,11 +23,16 @@ public class GameViewer {
     public static final int MAPVIEW_WIDTH = 400;
     public static final int MAPVIEW_HEIGHT = 400;
     
-    // holds reference to the runner and the Game
+    // Holds reference to the runner and the Game
+    // We need a reference to the runner because the runner handles all of the
+    // nasty I/O operations involving loading FXML files.
     private GSCQRunner runner;
     private Game owner;
     
-    // all of the FXML things
+    // The list of sprites currently needed on the floor
+    private ArrayList<EntitySprite> sprites = new ArrayList<>();
+    
+    // All of the FXML things
     @FXML
     private Label hpLabel;
     @FXML
@@ -36,10 +43,10 @@ public class GameViewer {
     private Label cookieLabel;
     @FXML
     private Label ivtLabel;
-	@FXML
-	private ImageView mapView;
-	@FXML
-	private AnchorPane spriteView;
+    @FXML
+    private ImageView mapView;
+    @FXML
+    private AnchorPane spriteView;
     
     @FXML
     public void displayMinimap() { runner.displayMinimap(); }
@@ -48,25 +55,44 @@ public class GameViewer {
     
     public void setupFloorView() {
         Image floorView = FloorViewBuilder.constructImage(owner.currentFloor());
+        for (GameEntity[] row : owner.getEntities()) {
+            for (GameEntity ge : row) {
+                if (ge != null) {
+                    EntitySprite es = new EntitySprite(ge);
+                    spriteView.getChildren().add(es);
+                    sprites.add(es);
+                }
+            }
+        }
         mapView.setImage(floorView);
         refreshMapview();
         refreshEntities();
     }
     
     public void refreshMapview() {
-        int tlX = owner.getPlayerXLoc() * FloorViewBuilder.TILE_DIMENSIONS;
-        int tlY = owner.getPlayerYLoc() * FloorViewBuilder.TILE_DIMENSIONS;
+        int tlX = owner.getPlayerXLoc() * ViewConstants.TILE_DIMENSIONS;
+        int tlY = (owner.getPlayerYLoc()-1) * ViewConstants.TILE_DIMENSIONS;
+        refreshEntities();
         mapView.setViewport(new Rectangle2D(
                 tlX, tlY, MAPVIEW_WIDTH, MAPVIEW_HEIGHT
         ));
     }
     
     public void refreshEntities() {
-        
+        Iterator<EntitySprite> it = this.sprites.iterator();
+        while (it.hasNext()) {
+            EntitySprite s = it.next();
+            if (!s.isInMap()) {
+                s.setVisible(false);
+                it.remove();
+                continue;
+            }
+            s.updatePosition();
+        }
     }
     
     public void updateFrame() {
-        
+        for (EntitySprite s : this.sprites) { s.refresh(); }
     }
     
     public void refreshHUD() {
@@ -78,7 +104,7 @@ public class GameViewer {
         String inventory="Inventory: \n";
         for (Item a : owner.getInventory()){
             if (a == null) { continue; }
-            inventory+=a.getItemID()+"\n";
+            inventory += a.getItemID()+"\n";
         }
         ivtLabel.setText(inventory);
     }
