57a01de932b3e35b0cbb5bf4f84b669cc01bc2ef
Cameron Wong <cam@lancer>
2015-11-17 17:03:33 -0500

    merge finally

--- src/edu/andover/cwong/gscq/view/ViewConstants.java
+++ src/edu/andover/cwong/gscq/view/ViewConstants.java
@@ -1,8 +1,8 @@
 package edu.andover.cwong.gscq.view;
 
 public class ViewConstants {
-    public static int TILE_DIMENSIONS = 67;
-    public static int TOP_LEFT_BLANKS = 3;
-    public static int BOT_RIGHT_BLANKS = 2;
-    public static int EDGE_BLANKS = TOP_LEFT_BLANKS + BOT_RIGHT_BLANKS;
+    public static final int TILE_DIMENSIONS = 67;
+    public static final int TOP_LEFT_BLANKS = 3;
+    public static final int BOT_RIGHT_BLANKS = 2;
+    public static final int EDGE_BLANKS = TOP_LEFT_BLANKS + BOT_RIGHT_BLANKS;
 }
