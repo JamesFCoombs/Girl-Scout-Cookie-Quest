f9daf68947bdd6aec2b81ffc924954185173aead
Cameron Wong <cam@lancer>
2015-11-17 17:00:06 -0500

    god dammit

---
+++ src/edu/andover/cwong/gscq/view/ViewConstants.java
@@ -0,0 +1,8 @@
+package edu.andover.cwong.gscq.view;
+
+public class ViewConstants {
+    public static int TILE_DIMENSIONS = 67;
+    public static int TOP_LEFT_BLANKS = 3;
+    public static int BOT_RIGHT_BLANKS = 2;
+    public static int EDGE_BLANKS = TOP_LEFT_BLANKS + BOT_RIGHT_BLANKS;
+}
