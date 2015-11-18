3c1f33cd9aca5437dd6491b37bc8484dd92aa9f7
Diana <fding@andover.edu>
2015-11-17 19:08:29 -0500

    not sure which other tests we should include

--- src/edu/andover/cwong/gscq/tests/GSCQTest.java
+++ src/edu/andover/cwong/gscq/tests/GSCQTest.java
@@ -20,17 +20,62 @@ public class GSCQTest {
 	}
 	
 	@Test
-	public void playerCannotBeOnTileImmediatelyBeyondTheWall() {
+	public void playerCannotBeOnTileImmediatelyBeyondTopWall() {
 		//To-do
 	}
 	
 	@Test
-	public void enemyCannotBeOnTileImmediatelyBeyondWall() {
+	public void playerCannotBeOnTileImmediatelyBeyondBottomWall() {
 		//To-do
 	}
 	
 	@Test
-	public void itemCannotBeOnTileImmediatelyBeyondWall() {
+	public void playerCannotBeOnTileImmediatelyBeyondRightWall() {
+		//To-do
+	}
+	
+	@Test
+	public void playerCannotBeOnTileImmediatelyBeyondLeftWall() {
+		//To-do
+	}
+	
+	@Test
+	public void enemyCannotBeOnTileImmediatelyBeyondTopWall() {
+		//To-do
+	}
+	
+	@Test
+	public void enemyCannotBeOnTileImmediatelyBeyondRightWall() {
+		//To-do
+	}
+	
+	@Test
+	public void enemyCannotBeOnTileImmediatelyBeyondBottomWall() {
+		//To-do
+	}
+	
+	@Test
+	public void enemyCannotBeOnTileImmediatelyBeyondLeftWall() {
+		//To-do
+	}
+	
+	@Test
+	public void itemCannotBeOnTileImmediatelyBeyondTopWall() {
+		//To-do
+	}
+	
+	@Test
+	public void itemCannotBeOnTileImmediatelyBeyondBottomWall() {
+		//To-do
+	}
+	
+	@Test
+	public void itemCannotBeOnTileImmediatelyBeyondRightWall() {
+		//To-do
+	}
+	
+	@Test
+	public void itemCannotBeOnTileImmediatelyBeyondLeftWall() {
 		//To-do
 	}
 	
@@ -63,6 +108,122 @@ public class GSCQTest {
 	public void everyLevelHasAnExit() {
 		//To-do
 	}
+	
+	@Test
+	public void playerGainsCookieOnFirstStep() {
+		//To-do
+	}
+	
+	@Test
+	public void playerExistsBeforeMovement() {
+		//To-do
+	}
+	
+	@Test
+	public void playerDoesNotLoseDamageTwoAwayFromEnenmy() {
+		//To-do
+	}
+	
+	@Test
+	public void playerDoesNotGainHPFromAttackingEnemyFromTop() {
+		//To-do
+	}
+	
+	@Test
+	public void playerDoesNotGainHPFromAttackingEnemyFromBelow() {
+		//To-do
+	}
+	
+	@Test
+	public void playerDoesNotGainHPFromAttackingEnemyFromLeft() {
+		//To-do
+	}
+	
+	@Test
+	public void playerDoesNotGainHPFromAttackingEnemyFromRight() {
+		//To-do
+	}
+	
+	@Test
+	public void playerLosesHPFromAttackingEnemyFromTop() {
+		//To-do
+	}
+	
+	@Test
+	public void playerLosesHPFromAttackingEnemyFromBelow() {
+		//To-do
+	}
+	
+	@Test
+	public void playerLosesHPFromAttackingEnemyFromRight() {
+		//To-do
+	}
+	
+	@Test
+	public void playerLosesHPFromAttackingEnemyFromLeft() {
+		//To-do
+	}
+	
+	@Test
+	public void enemyLosesHPFromAttackingPlayerFromTop() {
+		//To-do
+	}
+	
+	@Test
+	public void enemyLosesHPFromAttackingPlayerFromBelow() {
+		//To-do
+	}
+	
+	@Test
+	public void enemyLosesHPFromAttackingPlayerFromRight() {
+		//To-do
+	}
+	
+	@Test
+	public void enemyLosesHPFromAttackingPlayerFromLeft() {
+		//To-do
+	}
+	
+	@Test
+	public void playerCannotAttackEnemyTwiceInARow() {
+		//To-do
+	}
+	
+	@Test
+	public void enemyCannotAttackPlayerTwiceInARow() {
+		//To-do
+	}
+	
+	@Test
+	public void firstFloorHasAShop() {
+		//To-do
+	}
+	
+	@Test
+	public void secondFloorHasAShop() {
+		//To-do
+	}
+	
+	@Test
+	public void itemDisappearsAfterPlayerStepsOnItsTile() {
+		//To-do
+	}
+	
+	@Test
+	public void mascaraDoesNotAffectAttack() {
+		//To-do
+	}
+	
+	@Test
+	public void itemDisappearsFromInventoryOnceEquipped() {
+		//To-do
+	}
+	
+	@Test
+	public void effectOfFirstCookieStillLastsAfterSecondCookieIsFound() {
+		//To-do
+	}
+	
 }
 
 
